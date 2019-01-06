e35d98c18d52dae18f6fa0794f258ea76733a05e
Do Tuan Anh <dotuananh.dta@gmail.com>
2019-01-06 10:27:11 +0700

    add Marked.js dependency

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import marked from 'marked';
 import './App.css';
+import { defaultMarkdown } from './strings.js';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        <MarkdownInput />
+        <HtmlOutput />
       </div>
     );
   }
 }
+
+class MarkdownInput extends React.Component {
+  render() {
+    return (
+      <textarea id='markdown-input'>
+        {defaultMarkdown}
+      </textarea>
+    );
+  }
+}
+
+class HtmlOutput extends React.Component {
+  render() {
+    return (
+      <div id='html-output' dangerouslySetInnerHTML={{ __html: marked(defaultMarkdown)}} />
+    );
+  }
+}
+
 
 export default App;
